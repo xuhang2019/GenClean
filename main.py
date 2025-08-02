743d2bf36939d4a8217589f90af12e868e6babce
Aiden <xc369@cam.ac.uk>
2025-08-02 21:38:20 +0000

    refact: small typos

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import fire
 from utils import Config, Logger
 from models import Gencleaner
 from datasets import load_data
@@ -10,7 +9,6 @@ def main(
     use_revin=False,
     debug=False,
 ):
-    # 1. 配置
     config = Config(
         run_name=run_name,
         model_str=model_str,
@@ -22,17 +20,15 @@ def main(
     
     logger = Logger(config)
 
-    # 2. 数据
     train_data = load_data(config.train_data_path, subset='train', data_norm=config.data_norm)
     val_data = load_data(config.train_data_path, subset='val', data_norm=config.data_norm)
 
-    # 3. 模型
-    gencleaner = Gencleaner(config)
 
-    # 4. 训练
+    gencleaner = Gencleaner(config)
     gencleaner.fit(train_data, val_data, logger)
     logger.close()
 
 if __name__ == '__main__':
+    import fire
     fire.Fire(main)
 
